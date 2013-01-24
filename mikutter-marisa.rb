ca368a3aa31b7098e0323d452b497c3706414676
yukkuri_sinai <yukkuri.toho@gmail.com>
2013-01-24 23:39:39 +0900

    a lot of error

---
+++ mikutter-marisa.rb
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+
+Plugin.create(:marisa) do
+  #ツイートするテキスト
+  text={}
+  #作者リプ
+  sakusya_text="@yukkuri_sinai ねえなんでこんなキチクラ作ってるの？ねえねえ。魔理沙ちゃん大陸？こんなゴミクラ使えねーんだよｗｗｗｗｗ #Marisa_Tairiku"
+  #yakkai = Gtk::Button.new('厄介')
+  #sakusya = Gtk::Button.new('作者')
+  
+  #tab(:marisa, '魔理沙ちゃん大陸') do
+   # shrink
+    #nativewidget Gtk::HBox.new(false, 0).closeup(yakkai).closeup(sakusya)
+    #expand
+    #timeline :marisa
+  #end
+
+  #searchbtn.signal_connect('clicked'){ |elm|
+
+  #}
+
+end
